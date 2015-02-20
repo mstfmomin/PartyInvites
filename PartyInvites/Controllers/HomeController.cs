58525d28d78bcbb44ed9a3094822a5e0424a7371
mstfmomin <mstfmomin@hotmail.com>
2015-02-19 23:27:33 -0700

    Added a class and form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,6 +11,13 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
             return View();
         }
     }
